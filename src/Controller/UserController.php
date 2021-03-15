e911eb0f6f61dbe0827d2c2e2731404089fda59d
Leo <leo.cabral1016@gmail.com>
2021-03-15 19:47:51 -0300

    register form

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,13 +5,44 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Entity\User;
+use App\form\RegisterType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
-    public function register(): Response
-    {
+    public function register(Request $request, UserPasswordEncoderInterface $encoder): Response{
+
+        // Crear formulario
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        // Rellenar el objeto con los datos del form
+        $form->handleRequest($request);
+
+        // Comprobar si el form se ha enviado
+        if($form->isSubmitted() && $form->isValid()){
+
+            // Modificando el objeto para guardarlo
+            $user->setRole('role_user');
+            $user->setCreatedAt(new \Datetime('now'));
+
+            // Cifrar contraseña
+            $encoded = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($encoded);
+
+            // Guardar usuario
+            $em = $this->getdoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('tasks');
+        }
+
         return $this->render('user/register.html.twig', [
-            'controller_name' => 'UserController',
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Users
@@ -11,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="users")
  * @ORM\Entity
  */
-class User{
+class User implements UserInterface{
     # ATR
     #---------------------------------------------------#
         /**
@@ -34,6 +36,8 @@ class User{
          * @var string|null
          *
          * @ORM\Column(name="name", type="string", length=100, nullable=true)
+         * @Assert\NotBlank
+         * @Assert\Regex("/^[A-Za-z ]+$/")
          */
         private $name;
 
@@ -41,6 +45,8 @@ class User{
          * @var string|null
          *
          * @ORM\Column(name="surname", type="string", length=200, nullable=true)
+         * @Assert\NotBlank
+         * @Assert\Regex("/^[A-Za-z ]+$/")
          */
         private $surname;
 
@@ -48,6 +54,10 @@ class User{
          * @var string|null
          *
          * @ORM\Column(name="email", type="string", length=255, nullable=true)
+         * @Assert\NotBlank
+         * @Assert\Email(
+         *      message = "El email '{{ value }}' no es válido",
+         * )
          */
         private $email;
 
@@ -55,6 +65,7 @@ class User{
          * @var string|null
          *
          * @ORM\Column(name="password", type="string", length=255, nullable=true)
+         * @Assert\NotBlank
          */
         private $password;
 
@@ -130,11 +141,11 @@ class User{
             return $this;
         }
 
-        public function getCreatedAt(): ?\DateTimeInterface{
+        public function getCreatedAt(){
             return $this->createdAt;
         }
 
-        public function setCreatedAt(?\DateTimeInterface $createdAt): self{
+        public function setCreatedAt($createdAt): self{
             $this->createdAt = $createdAt;
 
             return $this;
@@ -145,6 +156,22 @@ class User{
          */
         public function getTasks(): Collection{
             return $this->tasks;
+        }
+
+        public function getUsername(){
+            return $this->email;
+        }
+
+        public function getSalt(){
+            return null;
+        }
+
+        public function getRoles(){
+            return array('role_user');
+        }
+
+        public function eraseCredentials(){
+            return null;
         }
 
     # FUN
