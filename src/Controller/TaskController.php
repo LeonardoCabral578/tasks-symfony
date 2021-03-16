ce1f0a9347e5dfcc638dfd04aa3c85af6f99a2bf
Leo <leo.cabral1016@gmail.com>
2021-03-16 01:59:57 -0300

    task edit

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -76,4 +76,32 @@ class TaskController extends AbstractController{
             'tasks' => $tasks
         ]);
     }
+
+    public function edit(Request $request, UserInterface $user, Task $task){
+
+        if(!$user || $user->getId() != $task->getUser()->getId()){
+            return $this->redirectToRoute('tasks');
+        }
+
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            //$task->setCreatedAt(new \Datetime('now'));
+            //$task->setUser($user);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+
+            return $this->redirect(
+                    $this->generateUrl('task_detail', ['id' => $task->getId()]));
+        }
+
+        return $this->render('task/creation.html.twig', [
+            'edit' => true,
+            'form' => $form->createView()
+        ]);
+    }
 }
